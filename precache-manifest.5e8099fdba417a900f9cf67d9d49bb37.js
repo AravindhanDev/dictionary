bde4db5fe857a4630ef9713db1f3341489cd6813
ADMIN <NoobieUs37@protonmail.com>
2021-10-17 19:07:57 +0530

    Updates

---
+++ precache-manifest.5e8099fdba417a900f9cf67d9d49bb37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec3f0afff610f0e4a10d2154439f566",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "d7a035da3d0a8af2417f",
+    "url": "/dictionary/static/js/2.2d265f19.chunk.js"
+  },
+  {
+    "revision": "010eea2e06b219ef58b9",
+    "url": "/dictionary/static/js/main.96afb611.chunk.js"
+  },
+  {
+    "revision": "983b7659eb5f622e230a",
+    "url": "/dictionary/static/js/runtime-main.fc794b76.js"
+  }
+]);
